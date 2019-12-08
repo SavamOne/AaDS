9ca5e808c8ef9a2598af66184a1a139507f778b5
Anton <anton222000@gmail.com>
2019-12-09 01:30:13 +0300

    Course Work Kruskal Algorithm

---
+++ CourseWork/MST.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseWork
+{
+    //Базовый класс остовного дерева
+    //Содержит объявления основных методов и полей, реализацию методов для вывода дерева, реализацию класса грани графа
+    abstract class MST
+    {
+        public class Edge : IComparable<Edge>
+        {
+            public string N1 { get; set; } //Как название для вершины можно использовать любую строку
+            public string N2 { get; set; }
+            public int Сost { get; set; }
+
+            public Edge(string n1, string n2, int cost)
+            {
+                N1 = n1;
+                N2 = n2;
+                Сost = cost;
+            }
+
+            public override string ToString() => $"{N1} - {N2} : {Сost}"; //Вывод грани графа
+            public int CompareTo(Edge other) => Сost.CompareTo(other.Сost); //Сортировку граней в списке/массиве осуществлять по значению стоимости
+        }
+
+        protected List<Edge> Graph { get; set; } //Граф - содержит все грани, которые добавил пользователь
+        protected List<Edge> Tree { get; set; } //Дерево - содержит только те грани, которые подходят для остовного дерева
+        public int Count => Tree.Count; //Количество элементов в дереве
+
+        public MST()
+        {
+            Graph = new List<Edge>();
+            Tree = new List<Edge>();
+        }
+
+        public abstract void AddEdge(string n1, string n2, int cost); //Добавить грань
+        public abstract void FindMST(); //Найти остовное дерево
+
+        public IEnumerator<Edge> GetEnumerator() => Tree.GetEnumerator();
+        public string this[int key] => Tree[key].ToString();
+    }
+}
---
+++ CourseWork/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CourseWork
+{
+    class Program
+    {
+        public static Tuple<string, string, int> Parse(string line)
+        {
+            string[] objects = line.Split(' ');
+            if (int.TryParse(objects[2], out int cost))
+                return Tuple.Create(objects[0], objects[1], cost);
+            else
+                throw new Exception("Третий объект был не числом");
+        }
+
+        static void Main(string[] args)
+        {
+            var MST = new KruskalMST();
+
+            while (true)
+            {
+                var objects = Parse(Console.ReadLine());
+
+                MST.AddEdge(objects.Item1, objects.Item2, objects.Item3);
+                MST.FindMST();
+
+                Console.WriteLine("-----");
+                int sum = 0;
+                foreach (MST.Edge edge in MST)
+                {
+                    Console.WriteLine(edge);
+                    sum += edge.Сost;
+                }
+                Console.WriteLine(sum);
+                Console.WriteLine("-----");
+            }
+        }
+    }
+}
